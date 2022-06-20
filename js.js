e82d16ef3ab154f9002da79b8d76967f7ee2c036
Ryan Zaleski <ryanzaleski@hotmail.co.uk>
2022-06-21 00:30:00 +0100

    completed function for game rules

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+/**** FUNCTIONS TO OPEN AND CLOSE HOW TO PLAY SECTION ****/
+
 document.querySelectorAll('.exit').forEach(elem => elem.addEventListener('click', hide))
 
 function hide(){
@@ -12,36 +14,89 @@ function show(){
 	document.querySelector('.gameSection').style.display = 'none'
 }
 
-let footballers = {
-	'Zlatan Ibrahimavic': {
-	'firstName': 'Zlatan',
-	'lastName': 'Ibrahimavic',
-	'listOfPlayers': ['Lionel Messi', 'Thiago', 'Juan Mata', 'Rafael van der Vaart', 'Patrick Vieira', 'Gianluigi Buffon', 'Blaise Matuidi', 'Thiago Silva', 'Edinson Cavani', 'Paul Pogba']
-}
-}
+/**** LIST OF FOOTBALLERS ****/
+
+let ibrahimavic = ['Lionel Messi', 'Thiago', 'Juan Mata', 'Rafael van der Vaart', 'Patrick Vieira', 'Gianluigi Buffon', 'Blaise Matuidi', 'Thiago Silva', 'Edinson Cavani', 'Paul Pogba']
+
+/**** LIST OF FIRST THREE PLAYERS ****/
+
+document.querySelector('#one').innerText = ibrahimavic[0]
+document.querySelector('#two').innerText = ibrahimavic[1]
+document.querySelector('#three').innerText = ibrahimavic[2]
 
 /* footballers["Zlatan Ibrahimavic"].listOfPlayers.forEach(element => console.log(element))
 console.log(footballers["Zlatan Ibrahimavic"].lastName) */
 
+/**** FUNCTION FOR GAME ****/
+
 document.querySelector('#button').addEventListener('click', result)
+
+let counter = 0
 
 function result(){
 	let inputedAnswer = document.querySelector('input').value
 	let ul = document.getElementById("list")
 	let li = document.createElement("li")
-	if (inputedAnswer.toLowerCase() === footballers["Zlatan Ibrahimavic"].lastName.toLowerCase()) {
+	
+    if (inputedAnswer.toLowerCase() !== 'ibrahimavic' ){
+		counter = counter + 1
+	} else { 
 		console.log('Correct')
-	} else {
-		li.appendChild(document.createTextNode(footballers["Zlatan Ibrahimavic"].listOfPlayers[+2]));
+	}
+
+
+	console.log(counter)
+
+	if (counter === 1) {
+		li.appendChild(document.createTextNode(ibrahimavic[3]));
 		li.setAttribute('id', 'four');
 		ul.appendChild(li)
 		console.log(li.id)
+	} else if (counter === 2){
+		li.appendChild(document.createTextNode(ibrahimavic[4]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
+	} else if (counter === 3){
+		li.appendChild(document.createTextNode(ibrahimavic[5]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
+	} else if (counter === 4){
+		li.appendChild(document.createTextNode(ibrahimavic[6]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
+	}  else if (counter === 5){
+		li.appendChild(document.createTextNode(ibrahimavic[7]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
+	} else if (counter === 6){
+		li.appendChild(document.createTextNode(ibrahimavic[8]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
+	} else if (counter === 7){
+		li.appendChild(document.createTextNode(ibrahimavic[9]));
+		li.setAttribute('id', 'five');
+		ul.appendChild(li)
+		console.log(li.id)
+
 	}
 }
+	
 
-document.querySelector('#one').innerText = footballers["Zlatan Ibrahimavic"].listOfPlayers[0]
-document.querySelector('#two').innerText = footballers["Zlatan Ibrahimavic"].listOfPlayers[1]
-document.querySelector('#three').innerText = footballers["Zlatan Ibrahimavic"].listOfPlayers[2]
+	
+
+
+
+
 
 
 
